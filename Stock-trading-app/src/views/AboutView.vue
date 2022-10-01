b6e22ea70d3458ffac6c0b98e08a056f630dbe52
Siddarth Kotthur <avigte@LAPTOP-JNT03HPL.localdomain>
2022-10-01 16:35:11 -0500

    Set basic routes and connected firebase

--- Stock-trading-app/src/views/AboutView.vue
+++ Stock-trading-app/src/views/AboutView.vue
@@ -5,11 +5,25 @@
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
+
+<script>
+  import { initializeApp } from "firebase/app";
+  import { getFirestore } from "firebase/firestore";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDFcXiGK1LQJDj1bkJN10bjKAflKNN3n3I",
+    authDomain: "vue-stuff-a1a86.firebaseapp.com",
+    databaseURL: "https://vue-stuff-a1a86-default-rtdb.firebaseio.com",
+    projectId: "vue-stuff-a1a86",
+    storageBucket: "vue-stuff-a1a86.appspot.com",
+    messagingSenderId: "200012668110",
+    appId: "1:200012668110:web:18c8c5d167d14eb46dc49f",
+    measurementId: "G-31C594NLNQ"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
+</script>
--- Stock-trading-app/src/views/HomeView.vue
+++ Stock-trading-app/src/views/HomeView.vue
@@ -1,9 +1,12 @@
+<!-- <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script> -->
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HelloWorld />
   </main>
 </template>
