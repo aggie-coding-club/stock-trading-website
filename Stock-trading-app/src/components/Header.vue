30626035e64ff4e6092373f1edab0b1aa52523a5
willheeney03 <99232572+willheeney03@users.noreply.github.com>
2022-10-23 20:03:51 -0500

    Created Basic Navbar
    features:
    navbar with links to pages
    highlights when you hover over

--- Stock-trading-app/src/components/Header.vue
+++ Stock-trading-app/src/components/Header.vue
@@ -1,11 +1,65 @@
 <!-- Header for all pages: Will, Keeney -->
 
-<script></script>
+<script>
+
+    export default {
+        // props: ['page'],
+        // created() {
+        //     // props are exposed on `this`
+        //     console.log(this.page);
+        //     // if (true) {
+        //     //     console.log(this.$ref.home1); 
+        //     // }
+        // },
+
+        // mounted: function() {
+        //     console.log(this.$ref.home); //deleted reference
+        // }
+    }
+
+</script>
 
 <template>
     <div class="Navbar">
 
+        <ul>
+            <li style="float:right"><a id="home" href="\">Home</a></li>
+            <li style="float:right"><a id="portfolio" href="\portfolio">Portfolio</a></li>
+            <li style="float:right"><a id="trading" href="\trade">Trading</a></li>
+            <li style="float:right"><a id="login" href="\user">Log In</a></li>
+        </ul>
     </div>
 </template>
 
-<style></style>
+
+
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    /* Change the link color to #111 (black) on hover */
+    li a:hover {
+        background-color: #111;
+    }
+
+    .active {
+        background-color: #800020;
+    }
+</style>
--- Stock-trading-app/src/views/Login.vue
+++ Stock-trading-app/src/views/Login.vue
@@ -1,12 +1,14 @@
-<script>
+<script setup>
 import { GoogleAuthProvider } from "firebase/auth";
+import Header from "../components/Header.vue"
+
 
 const provider = new GoogleAuthProvider();
 
 </script>
 
 <template>
-
+<Header/>
 </template>
 
 <style></style>
