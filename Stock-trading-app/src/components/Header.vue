9f228a2f0db17b34b77e45264ff64ba967e32dc1
Siddarth Kotthur <avigte@LAPTOP-JNT03HPL.localdomain>
2022-10-28 16:33:21 -0500

    reactive navbar

--- Stock-trading-app/src/components/Header.vue
+++ Stock-trading-app/src/components/Header.vue
@@ -1,65 +1,64 @@
 <!-- Header for all pages: Will, Keeney -->
 
 <script>
-
-    export default {
-        // props: ['page'],
-        // created() {
-        //     // props are exposed on `this`
-        //     console.log(this.page);
-        //     // if (true) {
-        //     //     console.log(this.$ref.home1); 
-        //     // }
-        // },
-
-        // mounted: function() {
-        //     console.log(this.$ref.home); //deleted reference
-        // }
-    }
-
+export default {
+  // props: ['page'],
+  // created() {
+  //     // props are exposed on `this`
+  //     console.log(this.page);
+  //     // if (true) {
+  //     //     console.log(this.$ref.home1);
+  //     // }
+  // },
+  // mounted: function() {
+  //     console.log(this.$ref.home); //deleted reference
+  // }
+};
 </script>
 
 <template>
-    <div class="Navbar">
-
-        <ul>
-            <li style="float:right"><a id="home" href="\">Home</a></li>
-            <li style="float:right"><a id="portfolio" href="\portfolio">Portfolio</a></li>
-            <li style="float:right"><a id="trading" href="\trade">Trading</a></li>
-            <li style="float:right"><a id="login" href="\user">Log In</a></li>
-        </ul>
-    </div>
+  <div class="Navbar">
+    <ul>
+      <li style="float: left"><a href="/">StockSim</a></li>
+      <li style="float: right" :class="this.$route.name == 'user' ? 'active' : ''">
+        <a id="login" href="\user">Log In</a>
+      </li>
+      <li style="float: right" :class="this.$route.name == 'trade' ? 'active' : ''">
+        <a id="trading" href="\trade">Trading</a>
+      </li>
+      <li style="float: right" :class="this.$route.name == 'portfolio' ? 'active' : ''">
+        <a id="portfolio" href="\portfolio">Portfolio</a>
+      </li>
+      <li style="float: right" :class="this.$route.name == 'home' ? 'active' : ''">
+        <a id="home" href="\">Home</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
-
-
 <style>
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: #333;
-    }
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
 
-    li {
-        float: left;
-    }
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
 
-    li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-    }
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
 
-    /* Change the link color to #111 (black) on hover */
-    li a:hover {
-        background-color: #111;
-    }
-
-    .active {
-        background-color: #800020;
-    }
+.active {
+  background-color: #800020;
+}
 </style>
