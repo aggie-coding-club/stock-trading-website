c8cc735a9bdd18843dc220456361c0db56af387d
anevanp <113946921+anevanp@users.noreply.github.com>
2022-10-29 23:00:49 -0500

    Update Heading.vue
    Added basic style to heading

--- Stock-trading-app/src/components/Heading.vue
+++ Stock-trading-app/src/components/Heading.vue
@@ -5,7 +5,30 @@
 <template>
 <div class="Heading">
     <h1>Stock Trading Simulator</h1>
+    <p>A project by Yonghwan Kim, Keegan Smith, Will Heeney, Evan Pan, Marco Verderame, Siddarth Kotthur, and Munish Shah.</p>
 </div>
 </template>
 
-<style></style>
+<!-- Basic heading style-->
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap');
+h1 {
+    font-family: 'Oswald', sans-serif;
+    text-align: center;
+    font-size: 300%;
+    color: white;
+    background-color: rgb(80, 0, 0);
+    padding-top: 50px;
+    padding-bottom: 10px;
+    margin: 0;
+}
+p {
+    font-family: 'Oswald', sans-serif;
+    text-align: center;
+    font-size: 100%;
+    color: white;
+    background-color: rgb(80, 0, 0);
+    padding-bottom: 50px;
+    margin-top: 0;
+}
+</style>
